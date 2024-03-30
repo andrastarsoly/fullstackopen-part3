ce4457f33b7ed7365e22cb315df49c61d7bf2ceb
andras_tarsoly <tarsoly.andras.mark@gmail.com>
2024-03-30 11:40:56 +0100

    part3.6

--- index.js
+++ index.js
@@ -48,6 +48,34 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
+app.post('/api/persons/', (request, response) => {
+    const body = request.body
+
+    if (!body.name || !body.number) {
+        return response.status(400).json({ 
+          error: 'The name or number is missing' 
+        })
+    }
+
+    if(persons.find(person => person.name === body.name)){
+        return response.status(400).json({ 
+        error: 'Name must be unique' 
+      })
+    }
+
+
+    const id = Math.floor(Math.random()*33333)
+    const person = {
+        id: id,
+        name: body.name,
+        number: body.number
+    }
+
+    persons = person.concat(person)
+    response.json(persons);
+})
+
+
 app.get('/info', (request, response) => {
     response.send(`Phonebook has info for ${persons.length} people <br/> ${new Date().toString()}`)
 })
@@ -55,4 +83,4 @@ app.get('/info', (request, response) => {
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
-})
+}) 
